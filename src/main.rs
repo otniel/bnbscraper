51a00e80b2d342aba0f42e21e70467ec03f5bbe1
Otniel Aguilar <otniel.aguilar@spectrumeffect.com>
2022-03-04 00:23:48 -0600

    Update scraper
    - Process urls with async/await
    - Save items into JSON
    - Some refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
+use std::fs::File;
 
 use color_eyre::Report;
+use futures::stream::FuturesUnordered;
+use futures::StreamExt;
 use reqwest::Client;
-use select::document::Document;
+use select::document::{Document, Find};
 use select::node::Node;
 use select::predicate::{Class, Name, Predicate};
+use serde::{Deserialize, Serialize};
 use tracing::info;
 use tracing_subscriber::EnvFilter;
 
 const ROOT_URL: &str = "https://www.bathandbodyworks.mx";
 
-#[derive(Debug, Default)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 struct BnBItem {
     name: String,
     item_type: String,
     link: String,
     price: f32,
+    price_promo: f32,
     discount: String,
+}
+
+impl PartialEq for BnBItem {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name && self.item_type == other.item_type
+    }
 }
 
 #[tokio::main]
@@ -30,83 +41,148 @@ async fn main() -> Result<(), Report> {
 
     let document = Document::from(res.as_str());
     let links = document.find(Name("a"));
+    let uniq_links: Vec<String> = get_unique_links(links);
 
-    let uniq_links: Vec<String> = links
-        .into_iter()
-        .map(|node| node.attr("href").unwrap())
-        .collect::<HashSet<_>>()
-        .into_iter()
-        .filter(|link| !link.contains("www"))
-        .map(|link| format!("{}{}", ROOT_URL, link))
-        .collect();
     info!("Landing page links fetched...");
 
     let mut all_items: Vec<BnBItem> = Vec::new();
+    let mut items_futures = uniq_links
+        .iter()
+        .map(|link| process_link(link))
+        .collect::<FuturesUnordered<_>>();
 
-    for link in uniq_links {
-        info!("Procesing link {}", &link);
-
-        let res = reqwest::get(link).await?.text().await?;
-        let document = Document::from(res.as_str());
-        let products = document.find(Class("product-item"));
-
-        for product in products {
-            let mut bnb_item = BnBItem::default();
-            process_item(
-                product,
-                Class("product-item__caption"),
-                Name("a"),
-                |caption: Node| {
-                    bnb_item.name = caption.text();
-                    bnb_item.link = caption.attr("href").unwrap().to_owned();
-                },
-            );
-            process_item(
-                product,
-                Class("product-item__form"),
-                Name("li"),
-                |item_type: Node| {
-                    bnb_item.item_type = item_type.text();
-                },
-            );
-            process_item(
-                product,
-                Class("product-item__price"),
-                Name("span"),
-                |price: Node| {
-                    let price = price.text().replace("$", "");
-                    let parsed_price = price.parse::<f32>();
-                    if let Ok(parsed_price) = parsed_price {
-                        bnb_item.price = parsed_price;
-                    }
-                },
-            );
-            process_item(
-                product,
-                Class("product-item__flags--discounts"),
-                Name("p"),
-                |discount: Node| {
-                    bnb_item.discount = discount.text();
-                },
-            );
-
-            all_items.push(bnb_item);
+    while let Some(result) = items_futures.next().await {
+        if let Ok(products) = result {
+            for product in products {
+                if !all_items.contains(&product) {
+                    all_items.push(product);
+                }
+            }
         }
     }
 
     info!("Finished!");
     info!("Total items: {}", all_items.len());
 
+    let mut grouped: HashMap<&str, Vec<&BnBItem>> = HashMap::new();
+    for item in all_items.iter() {
+        grouped
+            .entry(item.discount.as_str())
+            .or_insert_with(Vec::new)
+            .push(item);
+    }
+
+    let json_file = "/Users/otniel/Documents/code/rust/bnbscraper/data.json";
+    serde_json::to_writer(&File::create(json_file)?, &grouped)?;
+
     Ok(())
 }
 
-fn process_item(
-    item: Node,
-    class: Class<&str>,
-    descendant: Name<&str>,
-    mut handler: impl FnMut(Node),
-) {
-    let link_node = item.find(class.descendant(descendant)).next();
+async fn process_link(link: &str) -> Result<Vec<BnBItem>, Report> {
+    info!("Processing link: {}", link);
+    let res = reqwest::get(link).await?.text().await?;
+    let document = Document::from(res.as_str());
+    let products = document.find(Class("product-item"));
+
+    let mut products_in_link = vec![];
+    for product in products {
+        let mut bnb_item = BnBItem::default();
+        process_product(product, &mut bnb_item);
+
+        if !products_in_link.contains(&bnb_item) {
+            products_in_link.push(bnb_item);
+        }
+    }
+    Ok(products_in_link)
+}
+
+fn process_product(product: Node, mut bnb_item: &mut BnBItem) {
+    extract_name_and_link(product, &mut bnb_item);
+    extract_item_type(product, &mut bnb_item);
+    extract_price(product, &mut bnb_item);
+    extract_price_promo(product, &mut bnb_item);
+    extract_discount(product, &mut bnb_item);
+}
+
+fn get_unique_links(links: Find<Name<&str>>) -> Vec<String> {
+    links
+        .into_iter()
+        .map(|node| node.attr("href").unwrap())
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .filter(|link| !link.contains("www"))
+        .map(|link| format!("{}{}", ROOT_URL, link))
+        .collect()
+}
+
+fn extract_discount(product: Node, bnb_item: &mut BnBItem) {
+    process_attribute(
+        product,
+        Class("product-item__flags--discounts"),
+        Name("p"),
+        |discount: Node| {
+            bnb_item.discount = discount.text();
+        },
+    );
+}
+
+fn extract_price(product: Node, bnb_item: &mut BnBItem) {
+    process_attribute(
+        product,
+        Class("product-item__price"),
+        Name("span"),
+        |price: Node| {
+            let price = price.text().replace("$", "");
+            let parsed_price = price.parse::<f32>();
+            if let Ok(parsed_price) = parsed_price {
+                bnb_item.price = parsed_price;
+            }
+        },
+    );
+}
+
+fn extract_price_promo(product: Node, bnb_item: &mut BnBItem) {
+    process_attribute(
+        product,
+        Class("product-item__price"),
+        Class("price-new"),
+        |price: Node| {
+            let price = price.text().replace("$", "");
+            let parsed_price = price.parse::<f32>();
+            if let Ok(parsed_price) = parsed_price {
+                bnb_item.price_promo = parsed_price;
+            }
+        },
+    );
+}
+fn extract_item_type(product: Node, bnb_item: &mut BnBItem) {
+    process_attribute(
+        product,
+        Class("product-item__form"),
+        Name("li"),
+        |item_type: Node| {
+            bnb_item.item_type = item_type.text();
+        },
+    );
+}
+
+fn extract_name_and_link(product: Node, bnb_item: &mut BnBItem) {
+    process_attribute(
+        product,
+        Class("product-item__caption"),
+        Name("a"),
+        |caption: Node| {
+            bnb_item.name = caption.text();
+            bnb_item.link = caption.attr("href").unwrap().to_owned();
+        },
+    );
+}
+
+fn process_attribute<T>(item: Node, class: Class<&str>, predicate: T, mut handler: impl FnMut(Node))
+where
+    T: Predicate,
+{
+    let link_node = item.find(class.descendant(predicate)).next();
 
     if let Some(unwrapped_node) = link_node {
         handler(unwrapped_node);
